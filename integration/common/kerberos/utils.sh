b19f0a9e946e4cd3f1a1801bf303ea4e46067017
Saiguang Che <saiguang@alluxio.com>
2022-12-28 18:55:20 +0800

    try sth

---
+++ integration/common/kerberos/utils.sh
@@ -0,0 +1,18 @@
+copy_krb5_conf() {
+    user=$1
+    group=$2
+    f_mod="644"
+    d_mod="755"
+
+    cp /share/krb5/krb5.conf /etc/krb5.conf
+    chown $user:$group /etc/krb5.conf
+    chmod $f_mod /etc/krb5.conf
+
+    mkdir -p /etc/krb5.conf.d
+    chmod $d_mod /etc/krb5.conf.d
+
+    cp /share/krb5/krb5.conf.d/* /etc/krb5.conf.d/
+    chown $user:$group /etc/krb5.conf.d/*
+    chmod $f_mod /etc/krb5.conf.d/*
+
+}
---
+++ integration/hadoop/security/setup.sh
@@ -0,0 +1,4 @@
+# import kerberos related functions
+source ../../common/kerberos/utils.sh
+# copy krb5.conf files from container "kdc" to current container
+copy_krb5_conf root root
