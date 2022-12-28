c590c17e0d07a5afcfb40bf676bbe858c870ef9c
Saiguang Che <saiguang@alluxio.com>
2022-12-28 19:10:29 +0800

    try sth

--- integration/common/kerberos/utils.sh
+++ integration/common/kerberos/utils.sh
@@ -6,14 +6,14 @@ copy_krb5_conf() {
     f_mod="644"
     d_mod="755"
 
-    cp /share/krb5/krb5.conf /etc/krb5.conf
+    cp /share/krb5.conf /etc/krb5.conf
     chown $user:$group /etc/krb5.conf
     chmod $f_mod /etc/krb5.conf
 
     mkdir -p /etc/krb5.conf.d
     chmod $d_mod /etc/krb5.conf.d
 
-    cp /share/krb5/krb5.conf.d/* /etc/krb5.conf.d/
+    cp /share/krb5.conf.d/* /etc/krb5.conf.d/
     chown $user:$group /etc/krb5.conf.d/*
     chmod $f_mod /etc/krb5.conf.d/*
 
