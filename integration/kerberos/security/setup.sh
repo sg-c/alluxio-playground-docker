ae732c2931050fe36bbab5dbb0eec871b99d5e1a
Saiguang Che <saiguang@alluxio.com>
2022-12-28 19:21:19 +0800

    try sth

---
+++ integration/kerberos/security/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+# wait for kdc to start and ready
+while [ ! -f /kerberos_initialized ]; do
+    sleep 2
+done
+
+# copy krb5.conf files to /share
+cp /etc/krb5.conf /share/krb5.conf
+chmod 777 /share/krb5.conf
+
+mkdir -p /share/krb5.conf.d
+cp /etc/krb5.conf.d/* /share/krb5.conf.d/*
+chmod 777 /share/krb5.conf.d/*
--- scenario/security/integrate.sh
+++ scenario/security/integrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker exec security-kdc-1 /integration/hadoop/security/setup.sh
 docker exec security-namenode-1 /integration/hadoop/security/setup.sh
