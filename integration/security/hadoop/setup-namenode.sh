8e4601675b38b2954d280a2430d9685c55bca9e4
Saiguang Che <saiguang@alluxio.com>
2022-12-30 15:24:11 +0800

    try sth

--- integration/security/hadoop/setup-namenode.sh
+++ integration/security/hadoop/setup-namenode.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -xe
+
 source ./utils.sh
+source ../../common/kerberos/utils.sh
+
+# copy krb5.conf from KDC container
+copy_krb5_conf
 
 # copy new config files to $HADOOP_CONF_DIR
 copy_configs
