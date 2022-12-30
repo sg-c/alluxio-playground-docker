598cad78e6c8fff61b959272746d5e757af8d42b
Saiguang Che <saiguang@alluxio.com>
2022-12-30 17:14:42 +0800

    try sth

--- integration/security/hadoop/setup-datanode.sh
+++ integration/security/hadoop/setup-datanode.sh
@@ -8,7 +8,9 @@ source ./utils.sh
 copy_configs
 
 # create service principals for datanode
-create_service_princ dn     datanode
-create_service_princ host   datanode
+create_service_princ dn datanode
+create_service_princ host datanode
 
 # start datanode daemon
+HADOOP_LOG=${HADOOP_LOG_DIR}/hadoop-datanode.log
+nohup hdfs --daemon start datanode >${HADOOP_LOG} 2>&1 &
