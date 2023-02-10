ecdfdee39aa86e9a13987c151eb6e81ae2e0daee
Saiguang Che <saiguang@alluxio.com>
2023-02-10 13:39:54 +0800

    try sth

--- entrypoint/impala/entrypoint.sh
+++ entrypoint/impala/entrypoint.sh
@@ -5,5 +5,37 @@ set -xe
 # copy hive-site.xml to impala config dir
 cp /config/impala/hive-site.xml /opt/impala/conf
 
+case $(hostname -f) in
+*impalad*)
+    /opt/impala/bin/daemon_entrypoint.sh \
+        /opt/impala/bin/impalad \
+        -log_dir=/opt/impala/logs \
+        -abort_on_config_error=false \
+        -state_store_host=impala-statestored.alluxio.io \
+        -catalog_service_host=impala-catalogd.alluxio.io \
+        -mem_limit_includes_jvm=true \
+        -use_local_catalog=true \
+        -rpc_use_loopback=true \
+        -use_resolved_hostname=true
+    ;;
+*statestored*)
+    /opt/impala/bin/daemon_entrypoint.sh \
+        /opt/impala/bin/statestored \
+        -log_dir=/opt/impala/logs \
+        -use_resolved_hostname=true
+    ;;
+*catalogd*)
+    /opt/impala/bin/daemon_entrypoint.sh \
+        /opt/impala/bin/catalogd \
+        -log_dir=/opt/impala/logs \
+        -abort_on_config_error=false \
+        -state_store_host=statestored \
+        -catalog_topic_mode=minimal \
+        -hms_event_polling_interval_s=1 \
+        -invalidate_tables_on_memory_pressure=true \
+        -use_resolved_hostname=true
+    ;;
+esac
+
 # wait forever
 while true; do sleep 100; done
